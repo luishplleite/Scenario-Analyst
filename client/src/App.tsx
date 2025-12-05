de1ca6a48f797c87ae5c355fba1f88296ad02f4e
luishplleite <66848159+luishplleite@users.noreply.github.com>
2025-12-05 12:05:01 -0300

    Upload via API: client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+import { Switch, Route } from "wouter";
+>>>>>>> 68aad9d (Extracted stack files)
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+<<<<<<< HEAD
 import { GameProvider, useGame } from "@/lib/gameContext";
 import { LoginScreen } from "@/components/LoginScreen";
 import { LobbyScreen } from "@/components/LobbyScreen";
@@ -20,18 +25,36 @@ function GameRouter() {
     default:
       return <LoginScreen />;
   }
+=======
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      {/* Add pages below */}
+      {/* <Route path="/" component={Home}/> */}
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+>>>>>>> 68aad9d (Extracted stack files)
 }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+<<<<<<< HEAD
         <GameProvider>
           <div className="dark">
             <GameRouter />
           </div>
         </GameProvider>
         <Toaster />
+=======
+        <Toaster />
+        <Router />
+>>>>>>> 68aad9d (Extracted stack files)
       </TooltipProvider>
     </QueryClientProvider>
   );
